916d82ce008df1010c1ad09ec161e1f3665a3431
NguyenHungw <hwajio.o@gmail.com>
2025-12-31 23:52:07 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManhwaDimension.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
